6cbad8c55e016878b7f1fbb9cae20bc0519d8856
xianyuchen2077 <zhengyan24@mails.tsinghua.edu.cn>
2025-08-10 19:50:03 +0800

    1st version

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,13 @@
 # Add your commands here
+IMAGE_NAME="hw_copy_image"
+TAG="latest"
+
+docker build -t ${IMAGE_NAME}:${TAG} -f Dockerfile .
+
+echo "---运行 add.cpp 生成的 add 程序---"
+docker run --rm --name hw_copy_container ${IMAGE_NAME}:${TAG} add 10 5
+
+echo ""
+
+echo "---运行 mul.cpp 生成的 mul 程序---"
+docker run --rm --name hw_copy_container ${IMAGE_NAME}:${TAG} mul 10 5
